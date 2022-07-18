31a32d48688cc4817456c22aa92aeb1e1518c473
Tono <tonoyama0229@gmail.com>
2022-07-18 11:40:32 +0900

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className={"main"}>
       <Head>
         <title>Abstract</title>
-        <meta name='viewport' content='width=devise-width, initial-scale=1.0' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
 
       <div className="app_title" style={{color: "white"}}>
